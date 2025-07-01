f7c6f3e54f5d10633b970230e77c7811d964cf12
Harrison LaBollita <harrisonlabollita@gmail.com>
2025-07-01 19:16:13 -0400

    draft tutorial

---
+++ ModEST-DMFT/01-modest.py
@@ -0,0 +1,43 @@
+import numpy as np 
+from common import *
+import bzsummation as modest
+from triqs.gf import MeshImFreq, inverse
+from triqs.operators import n
+
+hdf5_filename = 'data/mlwf/lco_wannier.h5'
+target_density, obe = modest.one_body_elements_from_dft_converter(hdf5_filename)
+E = modest.make_default_embedding(obe.C_space)
+
+beta = 10.0 # inverse temperature
+mesh = MeshImFreq(beta, S='Fermion', n_iw=1025) # Matsubara mesh
+mu = modest.find_chemical_potential(target_density, obe, beta, verbosity=False) # verbosity broken!
+Gloc = E.extract(modest.gloc(mesh, obe, mu))[0]
+hloc0 = E.extract(modest.impurity_levels(obe)-mu)[0]
+Delta_iw = modest.extract_delta(hloc0, Gloc)
+
+U = 3.0
+h_int = U*n('up_0',0)*n('down_0',0)
+
+solver_params = dict(length_cycle=60, n_cycles = int(5e+5), 
+                          n_warmup_cycles = int(1e+4),
+                          perform_tail_fit=True, fit_min_w=6, fit_max_w=10,
+                          imag_threshold = 1e-6)
+
+n_dmft_loops = 10
+
+for n_iter in range(n_dmft_loops):
+    print(f"DMFT iteration= {n_iter}")
+    # solve!
+    solver_results = solve(Delta_iw, hloc0, h_int, **solver_params)
+    # update Sigma!
+    Sigma_C  = E.embed([ solver_results.Sigma_iw ]) 
+    # update mu!
+    mu     = modest.find_chemical_potential(target_density, obe, Sigma_C, verbosity=False)
+    # update Gloc!
+    Gloc   = E.extract(modest.gloc(obe, mu, Sigma_C, None))[0] # FIXME
+    # update hloc0
+    hloc0 = E.extract(modest.impurity_levels(obe)-mu)[0]
+    # update Δ!
+    Delta_iw = modest.extract_delta(hloc0, Gloc, solver_results.Sigma_iw)
+    print(f"Δn = |n_lattice - n_impurity| = {abs(Gloc.total_density()-solver_results.G_iw.total_density())}")
+    
---
+++ ModEST-DMFT/common.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+def __plot_band_structure(canvas, 
+                          kpts, bands,
+                          fermi_level=0.0, 
+                          high_symm_points= None,
+                          high_symm_labels= None
+                         ):
+    
+    for b in range(len(bands)): canvas.plot(kpts, bands[b, :]-fermi_level, 'k-', lw=1)
+    if high_symm_points:
+        for k in high_symm_points: canvas.axvline(k, color='k', ls='dotted', lw=0.5)
+    canvas.axhline(0.0 if fermi_level != 0 else fermi_level, color='k', ls='dotted', lw=0.5)
+    canvas.set_xticks(high_symm_points); canvas.set_xlim(min(high_symm_points), max(high_symm_points));
+    if high_symm_labels:
+        assert len(high_symm_labels) == len(high_symm_points)
+        canvas.set_xticklabels(high_symm_labels)
+    canvas.tick_params(axis='x', which='both', length=0)
+
+def __plot_bands(canvas, kpts, bands, **kwargs):
+    for b in range(len(bands)): canvas.plot(kpts, bands[b, :], '-', **kwargs)
+        
+mpl.axes.Axes.plot_band_structure = lambda self, kpts, bands, **kwargs: __plot_band_structure(self, kpts, bands, **kwargs)
+mpl.axes.Axes.plot_bands = lambda self, kpts, bands, **kwargs: __plot_bands(self, kpts, bands, **kwargs)
+
+from triqs.gf import MeshImFreq, Gf, BlockGf, Block2Gf, make_gf_from_fourier, fit_hermitian_tail, make_hermitian
+from triqs.gf.tools import make_zero_tail
+from triqs.operators import c_dag, c
+from triqs_cthyb import Solver
+
+class SolverResults(dict):
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+    def __getattr__(self, key): 
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(f"SolverResults has no property {key}")
+    def __repr__(self):
+        return "\n".join([f"{key:<12}" for key in self.keys() ])
+        
+    __str__ = __repr__
+
+def solve(Delta_iw, h_loc0, h_int, **solver_params):
+    
+    gf_struct = [(bl, gf.target_shape[0]) for (bl, gf) in Delta_iw]
+    
+    beta  = Delta_iw.mesh.beta
+    n_iw = len(Delta_iw.mesh) // 2
+    n_tau = 10*n_iw
+
+    solver_params['measure_density_matrix'] = True
+    solver_params['use_norm_as_weight']     = True
+
+    h_loc0_mat = {block : h_loc0[ibl].real for ibl, (block, _) in enumerate(gf_struct) }
+    c_dag_vec  = {block : np.matrix([[c_dag(block, o) for o in range(bl_size)]]) for block, bl_size in gf_struct }
+    c_vec      = {block : np.matrix([[c(block, o) for o in range(bl_size)]]) for block, bl_size in gf_struct }
+    h_loc0     = sum(c_dag_vec[block]*h_loc0_mat[block]*c_vec[block] for block, bl_size in gf_struct)[0,0]
+
+    S = Solver(gf_struct=gf_struct, beta=beta, n_iw=n_iw, n_tau=n_tau, delta_interface=True)
+
+    for block, delta in S.Delta_tau:
+        S.Delta_tau[block] << make_gf_from_fourier(
+            Delta_iw[block],                                          # Δ(iω)
+            S.Delta_tau[block].mesh,                                  # time mesh
+            fit_hermitian_tail(Delta_iw[block], 
+                               make_zero_tail(Delta_iw[block], 1))[0] # tail
+            )
+    S.solve(h_loc0=h_loc0, h_int=h_int, **solver_params)
+    return SolverResults(#Delta_tau   = S.Delta_tau, 
+                         G_iw = S.G_iw.copy(),
+                         #G_iw_raw = S.G_iw_raw,                      
+                         #G_moments = S.G_moments,
+                         #G_tau = S.G_tau, 
+                         #G_tau_accum = S.G_tau_accum, 
+                         #G_l = S.G_l,                  
+                         #Sigma_Hartree = S.Sigma_Hartree, 
+                         Sigma_iw = S.Sigma_iw.copy(), 
+                         #Sigma_iw_raw = S.Sigma_iw_raw, 
+                         #Sigma_moments = S.Sigma_moments,
+                         #O_tau = S.O_tau,    
+                         #G2_iw = S.G2_iw, 
+                         #G2_iw_nfft = S.G2_iw_nfft, 
+                         #G2_iw_ph = S.G2_iw_ph, 
+                         #G2_iw_ph_nfft = S.G2_iw_ph_nfft, 
+                         #G2_iw_pp = S.G2_iw_pp,
+                         #G2_iw_pp_nfft = S.G2_iw_pp_nfft, 
+                         #G2_iwll_ph = S.G2_iwll_ph, 
+                         #G2_iwll_pp = S.G2_iwll_pp,
+                         #G2_tau = S.G2_tau, 
+                         #asymmetry_G_tau = S.asymmetry_G_tau,
+                         #auto_corr_time =  S.auto_corr_time, 
+                         #average_order = S.average_order, 
+                         #average_sign = S.average_sign, 
+                         #density_matrix = S.density_matrix,
+                         #h_loc_diagonalization = S.h_loc_diagonalization, 
+                         #orbital_occupations = S.orbital_occupations, 
+                         #performance_analysis = S.performance_analysis, 
+                         #perturbation_order = S.perturbation_order, 
+                         #perturbation_order_total = S.perturbation_order_total,
+                        )
