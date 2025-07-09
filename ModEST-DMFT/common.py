69c0d118761287b8b640ee565e5bbbfde69f0a4f
Harrison LaBollita <harrisonlabollita@gmail.com>
2025-07-09 15:45:37 -0400

    iterate on tutorial

--- ModEST-DMFT/common.py
+++ ModEST-DMFT/common.py
@@ -23,9 +23,37 @@ def __plot_band_structure(canvas,
 
 def __plot_bands(canvas, kpts, bands, **kwargs):
     for b in range(len(bands)): canvas.plot(kpts, bands[b, :], '-', **kwargs)
+
+def __plot_spectral_function(canvas, Akw, k_lin, k_ticks, k_labels, **kwargs):
+    
+    data = canvas.imshow(A_kw.T.real, origin='lower', 
+                      aspect='auto', 
+                      extent=(min(k_lin), max(k_lin), -10,10),
+                      **kwargs
+                     )
+    canvas.set_xlim(min(k_lin), max(k_lin))
+    canvas.set_xticks(k_ticks)
+    canvas.set_xticklabels(k_labels)
+    canvas.set_ylabel(r'$\omega$')
+    canvas.tick_params(axis='x', which='both', length=0)
+    canvas.tick_params(axis='y', which='both', direction='out')
+
         
 mpl.axes.Axes.plot_band_structure = lambda self, kpts, bands, **kwargs: __plot_band_structure(self, kpts, bands, **kwargs)
 mpl.axes.Axes.plot_bands = lambda self, kpts, bands, **kwargs: __plot_bands(self, kpts, bands, **kwargs)
+mpl.axes.Axes.plot_spectral_function = lambda self, Akw, k_lin, k_ticks, k_labels, **kwargs : __plot_spectral_function(self, Akw, k_lin, k_ticks, k_labels, **kwargs)
+
+
+def momentum_resolved_spectral_function(e_k, mu, Sigma_w, broadening = 0.1j):
+    n_kpts  = len(e_k)
+    n_ws    = len(Sigma_w.mesh)
+    A_kw  = np.zeros((n_kpts, n_ws))
+    omegas = np.fromiter(Sigma_w.mesh, float)
+    for ik in range(n_kpts):
+        A_kw[ik, :] = -(1/np.pi)*np.linalg.inv((omegas[:, None, None] + mu + broadening - ek[ik,None, None] 
+                         - Sigma_w.data[:])).trace(axis1=1,axis2=2).imag
+    return A_kw
+
 
 from triqs.gf import MeshImFreq, Gf, BlockGf, Block2Gf, make_gf_from_fourier, fit_hermitian_tail, make_hermitian
 from triqs.gf.tools import make_zero_tail
@@ -45,9 +73,16 @@ class SolverResults(dict):
         
     __str__ = __repr__
 
+def matrix_to_many_body_operator(h_loc_matrix, gf_struct):
+    h_loc0_mat = {block : h_loc_matrix[ibl].real for ibl, (block, _) in enumerate(gf_struct) }
+    c_dag_vec  = {block : np.matrix([[c_dag(block, o) for o in range(bl_size)]]) for block, bl_size in gf_struct }
+    c_vec      = {block : np.matrix([[c(block, o) for o in range(bl_size)]]) for block, bl_size in gf_struct }
+    return sum(c_dag_vec[block]*h_loc0_mat[block]*c_vec[block] for block, bl_size in gf_struct)[0,0]
+
 def solve(Delta_iw, h_loc0, h_int, **solver_params):
     
     gf_struct = [(bl, gf.target_shape[0]) for (bl, gf) in Delta_iw]
+    h_loc0 = matrix_to_many_body_operator(h_loc0, gf_struct)
     
     beta  = Delta_iw.mesh.beta
     n_iw = len(Delta_iw.mesh) // 2
@@ -55,11 +90,6 @@ def solve(Delta_iw, h_loc0, h_int, **solver_params):
 
     solver_params['measure_density_matrix'] = True
     solver_params['use_norm_as_weight']     = True
-
-    h_loc0_mat = {block : h_loc0[ibl].real for ibl, (block, _) in enumerate(gf_struct) }
-    c_dag_vec  = {block : np.matrix([[c_dag(block, o) for o in range(bl_size)]]) for block, bl_size in gf_struct }
-    c_vec      = {block : np.matrix([[c(block, o) for o in range(bl_size)]]) for block, bl_size in gf_struct }
-    h_loc0     = sum(c_dag_vec[block]*h_loc0_mat[block]*c_vec[block] for block, bl_size in gf_struct)[0,0]
 
     S = Solver(gf_struct=gf_struct, beta=beta, n_iw=n_iw, n_tau=n_tau, delta_interface=True)
 
@@ -70,18 +100,23 @@ def solve(Delta_iw, h_loc0, h_int, **solver_params):
             fit_hermitian_tail(Delta_iw[block], 
                                make_zero_tail(Delta_iw[block], 1))[0] # tail
             )
+        
     S.solve(h_loc0=h_loc0, h_int=h_int, **solver_params)
+    Sigma_dynamic = S.Sigma_iw.copy()
+    for bl, g in Sigma_dynamic: Sigma_dynamic[bl] << g - S.Sigma_Hartree[bl]
+    
     return SolverResults(#Delta_tau   = S.Delta_tau, 
-                         G_iw = S.G_iw.copy(),
+                         G_iw = S.G_iw,
                          #G_iw_raw = S.G_iw_raw,                      
                          #G_moments = S.G_moments,
-                         #G_tau = S.G_tau, 
+                         G_tau = S.G_tau, 
                          #G_tau_accum = S.G_tau_accum, 
                          #G_l = S.G_l,                  
-                         #Sigma_Hartree = S.Sigma_Hartree, 
-                         Sigma_iw = S.Sigma_iw.copy(), 
+                         Sigma_Hartree = list(S.Sigma_Hartree.values()), 
+                         Sigma_iw = S.Sigma_iw,
+                         Sigma_dynamic = Sigma_dynamic,
                          #Sigma_iw_raw = S.Sigma_iw_raw, 
-                         #Sigma_moments = S.Sigma_moments,
+                         Sigma_moments = S.Sigma_moments,
                          #O_tau = S.O_tau,    
                          #G2_iw = S.G2_iw, 
                          #G2_iw_nfft = S.G2_iw_nfft, 
@@ -93,13 +128,13 @@ def solve(Delta_iw, h_loc0, h_int, **solver_params):
                          #G2_iwll_pp = S.G2_iwll_pp,
                          #G2_tau = S.G2_tau, 
                          #asymmetry_G_tau = S.asymmetry_G_tau,
-                         #auto_corr_time =  S.auto_corr_time, 
-                         #average_order = S.average_order, 
-                         #average_sign = S.average_sign, 
-                         #density_matrix = S.density_matrix,
-                         #h_loc_diagonalization = S.h_loc_diagonalization, 
-                         #orbital_occupations = S.orbital_occupations, 
-                         #performance_analysis = S.performance_analysis, 
-                         #perturbation_order = S.perturbation_order, 
-                         #perturbation_order_total = S.perturbation_order_total,
+                         auto_corr_time =  S.auto_corr_time, 
+                         average_order = S.average_order, 
+                         average_sign = S.average_sign, 
+                         density_matrix = S.density_matrix,
+                         h_loc_diagonalization = S.h_loc_diagonalization, 
+                         orbital_occupations = S.orbital_occupations, 
+                         performance_analysis = S.performance_analysis, 
+                         perturbation_order = S.perturbation_order, 
+                         perturbation_order_total = S.perturbation_order_total,
                         )
